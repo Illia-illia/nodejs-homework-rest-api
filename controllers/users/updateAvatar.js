2f6a9b417fb5c092e9cb1fb540c057183284600f
Illia-illia <takoexerrorx@gmail.com>
2023-01-18 22:11:57 +0200

    resize250

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,16 +1,30 @@
 const { User } = require('../../models');
 const path = require('path');
 const fs = require('fs/promises');
+const Jimp = require('jimp');
 const avatarDir = path.join(__dirname, '../../', 'public', 'avatars');
 
-const updateAvatar = async (res, req) => {
+const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
+
+  //   Jimp.read(tempUpload, (err, image) => {
+  //     if (err) throw err;
+  //     image
+  //       .resize(100, 100) // resize
+  //       .write(tempUpload); // save
+  //   });
 
   try {
-    const resultUpload = path.join(avatarDir, originalname);
+    const resultUpload = path.join(avatarDir, imageName);
 
-      await fs.rename(tempUpload, resultUpload);
-      const 
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join('public', 'avatars', imageName);
+    await User.findByIdAndUpdate(id, { avatarURL });
+    res.json({
+      avatarURL,
+    });
   } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
