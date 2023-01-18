e0d15c61e054b3c2faf4c5d565113049e2b64bac
Illia-illia <takoexerrorx@gmail.com>
2023-01-18 18:41:16 +0200

    finish hw4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,7 +12,6 @@ const auth = async (req, res, next) => {
     if (bearer !== 'Bearer') {
       throw new Unauthorized('Not authorized');
     }
-
     const { id } = jwt.verify(token, SECRET_KEY);
 
     const user = await User.findById(id);
